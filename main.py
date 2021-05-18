080ec07081148c7af6b023f34f83d72912abb7cd
st17180 <70176066+st17180@users.noreply.github.com>
2021-05-19 11:39:32 +1200

    add user config flags to mem/cpu

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import psutil
 
 # sys get
-class monitor():
-    
+class Monitor(): 
     # returns list of cpu usages
-    def cpu(self):
-        get = psutil.cpu_percent(interval=1,percpu=True)
-        core = 0
-        result = []
-        for item in get:
-            core = core + 1
-            c = (core, item)
-            result.append(c)
+    def cpu(self, dpc):
+        if dpc == "per_core":
+            get = psutil.cpu_percent(interval=1,percpu=True)
+            core = 0
+            result = []
+            for item in get:
+                core = core + 1
+                c = (core, item)
+                result.append(c)
+        elif dpc == "total":
+            get = psutil.cpu_percent(interval=1,percpu=False)
+            result = ("total", get)
+        else:
+            print("mon.cpu misconfigured")
         return result
-    
     # returns used and total memory
-    def mem(self):
+    def mem(self, size):
         get = psutil.virtual_memory()
-        total = round(get[0] / 1073741824, 2)
-        used = round(get[3] / 1073741824, 2)
-        result = (used, "gb of",  total)
+        if size == "mb":
+            result = (round(get[3] / 1000000, 2), round(get[0] / 1000000, 2))
+        elif size == "gb":
+            result = (round(get[3] / 1073741824, 2), round(get[0] / 1073741824, 2))
+        else:
+            print("mon.mem misconfigured")
         return result
-
     # returns used and total disk
     def disk(self):
         get = psutil.disk_usage('/')
@@ -31,16 +37,16 @@ class monitor():
         return result
 
 # shorten class name
-mon = monitor()
+mon = Monitor()
 
 # CLI MENU
 x = 0
 while x < 1:
     awn = str(input("cpu, mem, disk or help : "))
     if awn == "cpu":
-        print(mon.cpu())
+        print(mon.cpu("per_core"))
     elif awn == "mem":
-        print(mon.mem())
+        print(mon.mem("mb"))
     elif awn == "disk":
         print(mon.disk())
     elif awn == "help":
@@ -49,4 +55,3 @@ while x < 1:
         x = 1
     else:
         continue
-# CLI MENU END
